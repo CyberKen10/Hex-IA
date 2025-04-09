af029ff100b578ec7dcbc7313cfbf3b8222f1cc4
Kendry <132113206+CyberKen10@users.noreply.github.com>
2025-04-09 11:34:47 -0400

    feat:Added the board

---
+++ player.py
@@ -0,0 +1,21 @@
+from hex_board import HexBoard
+import math
+import random
+
+# Definición de la clase base Player
+# Esta clase representa un jugador en el juego Hex.
+class Player:
+    def __init__(self, player_id: int):
+        self.player_id = player_id  # 1 (rojo) or 2 (azul)
+
+    def play(self, board: "HexBoard") -> tuple:
+        raise NotImplementedError("Implement this method!")
+    
+
+class IAPlayer(Player):
+    def __init__(self, player_id: int):
+         self.player_id = player_id  # 1 (rojo) or 2 (azul)
+
+    def play(self, board: HexBoard) -> tuple:
+       raise NotImplementedError("Implement this method!")
+    
